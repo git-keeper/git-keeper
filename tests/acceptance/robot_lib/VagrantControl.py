7f1c1c79dde7524f91a0e2996d6924fbbb03e22b
Ben Coleman <colemanb@moravian.edu>
2018-04-21 13:49:23 -0400

    Add console logging for long steps and checks for whether gkserver and gkclient are running.

--- tests/acceptance/robot_lib/VagrantControl.py
+++ tests/acceptance/robot_lib/VagrantControl.py
@@ -1,6 +1,6 @@
 import vagrant
 from gkeepcore.shell_command import run_command, run_command_in_directory
-
+from robot.api import logger
 
 class VagrantControl:
 
@@ -12,16 +12,26 @@ class VagrantControl:
         names = [box.name for box in self.v.box_list()]
 
         if 'gkserver' not in names:
+            logger.console('Making gkserver.box.  This can take up to 30 minutes but only executes once per system.')
             run_command_in_directory('gkserver_base', 'bash -c ./make_box.sh')
             self.v.box_add('gkserver', 'gkserver_base/gkserver.box')
 
         if 'gkclient' not in names:
+            logger.console('Making gkclient.box.  This can take up to 30 minutes but only executes once per system.')
             run_command_in_directory('gkclient_base', 'bash -c ./make_box.sh')
             self.v.box_add('gkclient', 'gkclient_base/gkclient.box')
 
     def start_vagrant(self, vagrant_start):
         if vagrant_start == 'Yes':
+            if self.is_server_running():
+                raise AssertionError('gkserver already running!')
+            if self.is_client_running():
+                raise AssertionError('gkclient already running!')
+            logger.console('Starting gkserver and gkclient VMs...')
             self.v.up()
+        else:
+            self.check_status()
+            logger.console('Using existing gkserver and gkclient VMS...')
 
     def start_gkeepd(self):
         self.run_on_server('keeper', 'screen -S gkserver -d -m gkeepd')
@@ -31,7 +41,10 @@ class VagrantControl:
 
     def stop_vagrant(self, vagrant_stop):
         if vagrant_stop == 'Yes':
+            logger.console('Destroying gkserver and gkclient VMs...')
             self.v.destroy()
+        else:
+            logger.console('Leaving gkserver and gkclient VMs running...')
 
     def expect_email(self, to_user, contains):
         result = self.run_server_script('keeper', 'email_check.py', to_user, contains)
@@ -71,14 +84,20 @@ class VagrantControl:
         full_cmd = ' '.join([base, port, user_and_key, suppress_warnings, cmd])
         return run_command(full_cmd)
 
+    def is_server_running(self):
+        result = self.v.status(vm_name='gkserver')
+        return result[0].state == 'running'
+
+    def is_client_running(self):
+        result = self.v.status(vm_name='gkclient')
+        return result[0].state == 'running'
+
     def check_status(self):
         if self.up_verified:
             return
-        result = self.v.status(vm_name='gkserver')
-        if result[0].state != 'running':
+        if not self.is_server_running():
             raise AssertionError('gkserver not running')
-        result = self.v.status(vm_name='gkclient')
-        if result[0].state != 'running':
+        if not self.is_client_running():
             raise AssertionError('gkclient not running')
         self.up_verified = True
 
