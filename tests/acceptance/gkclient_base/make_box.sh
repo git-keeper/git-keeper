dc8034e07212243b67afdf72bbba28180d7393d6
Ben Coleman <colemanb@moravian.edu>
2018-04-27 18:11:15 -0400

    Install git on gkserver and remove zero-out step in box build.

--- tests/acceptance/gkclient_base/make_box.sh
+++ tests/acceptance/gkclient_base/make_box.sh
@@ -1,7 +1,4 @@
 vagrant up
-echo 'Optimizing image...'
-vagrant ssh -- sudo dd if=/dev/zero of=/EMPTY bs=1M
-vagrant ssh -- sudo rm -f /EMPTY
 rm gkclient.box
 vagrant package --output gkclient.box
 vagrant destroy -f
