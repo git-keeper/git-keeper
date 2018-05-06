39c17bfeff16a620b230a8bec9c2b7f22dc5abb4
Nathan Sommer <nsommer@wooster.edu>
2018-05-05 22:21:20 -0400

    Add documentation

--- git-keeper-server/gkeepserver/faculty.py
+++ git-keeper-server/gkeepserver/faculty.py
@@ -1,4 +1,4 @@
-# Copyright 2016 Nathan Sommer and Ben Coleman
+# Copyright 2016, 2018 Nathan Sommer and Ben Coleman
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,14 +14,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-Provides a class for representing a faculty member.
-
+Provides a class for representing a faculty member, and a class for working
+with all faculty members.
 """
+
 import json
 import os
 from functools import wraps
 
-from gkeepcore.csv_files import CSVReader
 from gkeepcore.gkeep_exception import GkeepException
 from gkeepcore.system_commands import user_exists
 from gkeepserver.create_user import create_user, UserType
@@ -73,8 +73,26 @@ class Faculty:
                                              self.username, self.email_address)
 
 def raise_if_faculty_does_not_exist(func):
+    """
+    Decorator for FacultyMembers class methods that should not be called if a
+    certain faculty username does not exist. Raises a FacultyError if the given
+    username does not exist.
+
+    :param func: the function to decorate
+    :return: the new wrapped function
+    """
     @wraps(func)
     def wrapper(self, username, *args, **kwargs):
+        """
+        Check to see that the faculty username exists.
+
+        :param self: reference to a FacultyMembers object
+        :param username: faculty username to check
+        :param args: positional arguments to pass on
+        :param kwargs: keyword arguments to pass on
+        :return:
+        """
+
         if username not in self.json_dictionary:
             raise FacultyError('There is no faculty with the username {}'
                                .format(username))
@@ -85,7 +103,14 @@ def raise_if_faculty_does_not_exist(func):
 
 
 class FacultyMembers:
+    """
+    This class provides functionality for adding faculty members and getting
+    information about existing faculty members.
+    """
     def __init__(self):
+        """
+        Load the current stored faculty information.
+        """
         if not os.path.isfile(config.faculty_json_path):
             raise FacultyError('{} does not exist'
                                .format(config.faculty_json_path))
@@ -98,12 +123,22 @@ class FacultyMembers:
         except (json.JSONDecodeError, OSError) as e:
             raise FacultyError(e)
 
-    def write_json(self):
+    def write_faculty_information(self):
+        """
+        Write the in-memory faculty information to disk.
+        """
         with open(config.faculty_json_path, 'w') as f:
             json.dump(self.json_dictionary, f)
 
     def add_faculty(self, last_name, first_name, email_address, admin=False):
-        print(last_name, first_name, email_address, admin)
+        """
+        Add a faculty user.
+
+        :param last_name: last name of the faculty member
+        :param first_name: first name of the faculty member
+        :param email_address: email address of the faculty member
+        :param admin: True if the faculty member should be an admin
+        """
 
         try:
             username, _ = email_address.split('@')
@@ -129,39 +164,86 @@ class FacultyMembers:
             gkeepd_logger.log_debug('User created')
 
         self.json_dictionary[username] = faculty_dictionary
-        self.write_json()
+        self.write_faculty_information()
 
     @raise_if_faculty_does_not_exist
     def promote_to_admin(self, username):
+        """
+        Make a faculty member an admin user.
+
+        :param username: username of the faculty member
+        """
         self.json_dictionary[username]['admin'] = True
-        self.write_json()
+        self.write_faculty_information()
 
     @raise_if_faculty_does_not_exist
     def demote_from_admin(self, username):
+        """
+        Make a faculty member not an admin.
+
+        :param username: username of the faculty member
+        """
         self.json_dictionary[username]['admin'] = False
-        self.write_json()
+        self.write_faculty_information()
 
     def faculty_exists(self, username) -> bool:
+        """
+        Check if a faculty username exists.
+
+        :param username: username of the faculty member
+        :return: True if the username exists, False otherwise
+        """
         return username in self.json_dictionary
 
     @raise_if_faculty_does_not_exist
     def get_email(self, username) -> str:
+        """
+        Get the email address of a faculty member.
+
+        :param username: username of the faculty member
+        :return: the faculty's email address
+        """
         return self.json_dictionary[username]['email']
 
     @raise_if_faculty_does_not_exist
     def get_first_name(self, username) -> str:
+        """
+        Get the first name of a faculty member.
+
+        :param username: username of the faculty member
+        :return: the faculty's first name
+        """
         return self.json_dictionary[username]['first_name']
 
     @raise_if_faculty_does_not_exist
     def get_last_name(self, username) -> str:
+        """
+        Get the last name of a faculty member.
+
+        :param username: username of the faculty member
+        :return: the faculty's last name
+        """
         return self.json_dictionary[username]['last_name']
 
     @raise_if_faculty_does_not_exist
     def is_admin(self, username) -> bool:
+        """
+        Determine if a faculty member is an admin.
+
+        :param username: username of the faculty member
+        :return: True if the faculty member is an admin, False if not
+        """
         return self.json_dictionary[username]['admin']
 
     @raise_if_faculty_does_not_exist
     def get_faculty_object(self, username) -> Faculty:
+        """
+        Get a Faculty object representing the faculty member with the given
+        username.
+
+        :param username: username of the faculty member
+        :return: Faculty object representing the faculty member
+        """
         last_name = self.json_dictionary[username]['last_name']
         first_name = self.json_dictionary[username]['first_name']
         email_address = self.json_dictionary[username]['email_address']
@@ -170,6 +252,12 @@ class FacultyMembers:
         return Faculty(last_name, first_name, username, email_address, admin)
 
     def get_faculty_objects(self) -> list:
+        """
+        Get a list of Faculty objects representing all of the faculty members.
+
+        :return: list of Faculty objects
+        """
+
         faculty_objects = []
 
         for username in self.json_dictionary:
