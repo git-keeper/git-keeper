08ccb4f5490293f8b4a6827e42d468125078751f
Nathan Sommer <nsommer@wooster.edu>
2020-09-24 10:27:22 -0400

    Fix email about disabled assignment
    Previously the names of the assignment and the class were not actually inserted
    into the body of the email about a disabled assignment.

--- git-keeper-server/gkeepserver/event_handlers/disable_handler.py
+++ git-keeper-server/gkeepserver/event_handlers/disable_handler.py
@@ -73,7 +73,8 @@ class DisableHandler(EventHandler):
                              .format(self._class_name, self._assignment_name))
             email_body = ('Assignment {} in class {} has been disabled. '
                           'No tests will be run if you push to your '
-                          'repository for this assignment.')
+                          'repository for this assignment.\n'
+                          .format(self._assignment_name, self._class_name))
             for student in db.get_class_students(self._class_name,
                                                  self._faculty_username):
                 email_sender.enqueue(Email(student.email_address,
