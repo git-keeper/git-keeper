3d2d3849cc4f6d7eef4a0b0a83791051c3690b27
Nathan Sommer <sommern1@xavier.edu>
2022-07-22 22:11:36 -0400

    Do not info scan after testing faculty submissions
    Trying to do an info scan after a facutly submission raised an error
    which was benign but confusing.

--- git-keeper-server/gkeepserver/submission.py
+++ git-keeper-server/gkeepserver/submission.py
@@ -35,6 +35,7 @@ from gkeepcore.test_env_yaml import TestEnv, TestEnvType
 from gkeepcore.system_commands import cp, sudo_chown, rm, chmod, mv
 from gkeepcore.shell_command import run_command
 from gkeepserver.email_sender_thread import email_sender
+from gkeepserver.info_update_thread import info_updater
 from gkeepserver.reports import reports_clone
 from gkeepserver.server_configuration import config
 from gkeepserver.server_email import Email
@@ -165,6 +166,10 @@ class Submission:
 
             if self.student.username != faculty_username:
                 self._add_report(body)
+                info_updater.enqueue_submission_scan(self.faculty_username,
+                                                     self.class_name,
+                                                     self.assignment_name,
+                                                     self.student.username)
 
         except Exception as e:
             report_failure(assignment_name, self.student,
