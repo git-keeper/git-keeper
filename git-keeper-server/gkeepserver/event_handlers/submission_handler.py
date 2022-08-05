de8494923503517176a3a5c48cabe69170380b46
Ben Coleman <colemanb@moravian.edu>
2022-08-05 15:12:57 -0400

    remove check (it is handled in submission)

--- git-keeper-server/gkeepserver/event_handlers/submission_handler.py
+++ git-keeper-server/gkeepserver/event_handlers/submission_handler.py
@@ -63,20 +63,11 @@ class SubmissionHandler(EventHandler):
                                                        self._class_name,
                                                        self._faculty_username)
 
-        if db.is_disabled(self._class_name, self._assignment_name,
-                          self._faculty_username):
-            email_subject = ('[{}] Assignment disabled: {}'
-                             .format(self._class_name, self._assignment_name))
-            email_body = ('Assignment {} in class {} has been disabled.'
-                          .format(self._assignment_name, self._class_name))
-            email_sender.enqueue(Email(student.email_address,
-                                       email_subject, email_body))
-        else:
-            submission = Submission(student, self._submission_repo_path,
-                                    self._commit_hash, assignment_directory,
-                                    self._faculty_username, faculty_email)
+        submission = Submission(student, self._submission_repo_path,
+                                self._commit_hash, assignment_directory,
+                                self._faculty_username, faculty_email)
 
-            new_submission_queue.put(submission)
+        new_submission_queue.put(submission)
 
     def __repr__(self) -> str:
         """
